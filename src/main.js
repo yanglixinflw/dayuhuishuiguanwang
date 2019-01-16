a4a562d6aee5078751c4290cda90fea85c65f373
zxj462602 <z14758078652@163.com>
2019-01-16 16:54:30 +0800

    全局头部

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 // import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
-import { Row ,Col} from 'element-ui';
+import { Row,Col,MenuItem,Menu } from 'element-ui';
 
 Vue.config.productionTip = false
 // Vue.use(ElementUI);
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
 new Vue({
   router,
   render: h => h(App)
