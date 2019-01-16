7426b48c123a2621968b4ae641fb63cdb184222d
cph-code <cheng19970210@qq.com>
2019-01-16 17:48:29 +0800

    删除APP通用样式

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,10 @@
     <div id="content">
       <router-view/>
     </div>
-    
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 #content{
   margin-top: 68px;
 }
---
+++ src/views/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <IndexPage />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import IndexPage from "@/components/IndexPage/IndexPage.vue";
+export default {
+  name: "indexPage",
+  components: {
+    IndexPage
+  }
+};
+</script>
