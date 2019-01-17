77ebfcf94c3eddadfa5ebfb4db9289518927cc8d
cph-code <cheng19970210@qq.com>
2019-01-17 10:57:28 +0800

    拉取代码

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import App from './App.vue'
 // import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
-import { Row,Col,MenuItem,Menu } from 'element-ui';
+import { Row,Col,MenuItem,Menu, Carousel,CarouselItem } from 'element-ui';
 // Vue.config.productionTip = false
 // Vue.use(ElementUI);
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Menu)
 Vue.use(MenuItem)
+Vue.use(Carousel)
+Vue.use(CarouselItem);
 new Vue({
   router,
   render: h => h(App)
