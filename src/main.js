f0eedbe7e4d8f8a2af588339042bd0ecd3f8ab97
13223656899yx <yanglixinflw@gmail.com>
2019-01-17 14:08:59 +0800

    公司介绍

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 // import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
-import { Row,Col,MenuItem,Menu } from 'element-ui';
+import { Row,Col,MenuItem,Menu,Tabs,TabPane } from 'element-ui';
 // Vue.config.productionTip = false
 // Vue.use(ElementUI);
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Menu)
+Vue.use(Tabs)
+Vue.use(TabPane)
 Vue.use(MenuItem)
 new Vue({
   router,
