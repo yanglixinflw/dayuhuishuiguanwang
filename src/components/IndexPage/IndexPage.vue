3c7bd7a642e3d2fe2344c040ee23f2785b6a28f4
cph-code <cheng19970210@qq.com>
2019-01-20 15:31:12 +0800

    设置最小宽度为1200px,添加限制方法

--- src/components/IndexPage/IndexPage.vue
+++ src/components/IndexPage/IndexPage.vue
@@ -1,60 +1,88 @@
 <template>
   <div class="block">
-    <el-carousel trigger="click" height="880px">
+    <!-- 初始化height根据屏幕宽度设置 -->
+    <el-carousel trigger="click" :height="carouselHeight" :interval="3000">
       <el-carousel-item v-for="item in imgUrl" :key="item">
-        <img :src="item" alt=""/>
+        <img :src="item" alt>
       </el-carousel-item>
     </el-carousel>
   </div>
 </template>
-
 <style lang="less">
-.block{
-  // .el-carousel__item:nth-child(2n) {
-  //    background-color: #99a9bf;
-  // }
-  // .el-carousel__item:nth-child(2n+1) {
-  //    background-color: #d3dce6;
-  // }
-  img{
-    height: 880px;
+.block {
+  img {
+    height: 100%;
     width: 100%;
   }
-  .el-carousel__indicator{
-    padding: 12px 44px
+  .el-carousel__indicator {
+    padding: 12px 44px;
   }
-  .el-carousel__button{
+  .el-carousel__button {
     width: 60px;
     height: 4px;
     border-radius: 2px;
   }
+  .el-carousel__indicator.is-active button {
+    background: #1182f0;
+  }
   // .el-carousel__button :active{
   //   background: red;
   // }
-  .el-carousel__arrow{
+  .el-carousel__arrow {
     height: 60px;
-    width:60px;
-    background: rgba(219,219,219,0.6);
-    i{
+    width: 60px;
+    background: rgba(219, 219, 219, 0.6);
+    i {
       font-size: 24px;
     }
   }
 }
-
 </style>
 
 <script>
 export default {
   name: "IndexPage",
-  data :function (){
+  data: function() {
     // console.log( require('../../assets/carousel1.jpg'))
-    return{
-      imgUrl:[
-      require('../../assets/carousel1.jpg'),
-      require('../../assets/carousel2.jpg'),
-      require('../../assets/carousel3.jpg')]
+    return {
+      imgUrl: [
+        require("../../assets/carousel1.jpg"),
+        require("../../assets/carousel2.jpg"),
+        require("../../assets/carousel3.jpg")
+      ],
+      // 轮播图高度
+      carouselHeight: ((window.screenWidth * 880) / 1900).toFixed(1) + "px",
+      // 屏幕宽度
+      screenWidth: ""
+    };
+  },
+  mounted() {
+    let clientWidth = document.documentElement.clientWidth;
+    this.carouselHeight = ((clientWidth * 880) / 1900).toFixed(1) + "px";
+    this.screenWidth = clientWidth;
+    const that = this;
+    window.onresize = () => {
+      return (() => {
+        // window.screenWidth = document.body.clientWidth;
+        that.screenWidth = document.body.clientWidth;
+        // that.carouselHeight =
+        //   ((that.screenWidth * 880) / 1900).toFixed(1) + "px";
+      })();
+    };
+  },
+  watch: {
+    // 监听屏幕宽度属性变化
+    screenWidth(val)  {
+      console.log(val)
+      if(val>=1200){
+        this.carouselHeight =
+        ((val * 880) / 1900).toFixed(1) + "px";
+      }else{
+         this.carouselHeight =
+        ((1200 * 880) / 1900).toFixed(1) + "px";
+      }
+      
     }
   }
 };
-
 </script>
